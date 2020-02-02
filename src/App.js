64c445f234c3bedce9e6325d9f269703ff748961
Yang <zy_graysky@hotmail.com>
2020-02-02 20:50:15 +0100

    remove initial css, add gh-page dependency

--- src/App.js
+++ src/App.js
@@ -1,12 +1,10 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <img src="./logo192.png" className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
