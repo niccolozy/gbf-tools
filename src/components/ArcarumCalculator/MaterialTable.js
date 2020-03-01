b8e1dd01b7a8086c24de9bfd5e7bad2c6551bdf9
Yang <zy_graysky@hotmail.com>
2020-03-01 14:13:27 +0100

    Adjust MaterialTable colum width

--- src/components/ArcarumCalculator/MaterialTable.js
+++ src/components/ArcarumCalculator/MaterialTable.js
@@ -13,9 +13,14 @@ import { makeStyles } from "@material-ui/core/styles";
 
 const useStyles = makeStyles(theme => ({
   cell: {
-    padding: "6px"
+    padding: "6px",
+    width: "20%"
   },
   iconCell: {
+    padding: "6px",
+    width: "60%"
+  },
+  iconDiv: {
     display: "flex",
     alignItems: "center"
   },
@@ -32,7 +37,7 @@ const useStyles = makeStyles(theme => ({
   }
 }));
 
-export default function MaterialEstimation({
+export default function MaterialTable({
   type,
   materials,
   inventory,
@@ -44,13 +49,13 @@ export default function MaterialEstimation({
       <Table size="small">
         <TableHead>
           <TableRow>
-            <TableCell>
+            <TableCell className={classes.iconCell}>
               <b>{localeStrings.materialType[type].zh}</b>
             </TableCell>
-            <TableCell align="center">
+            <TableCell align="left" className={classes.cell}>
               <b>已有</b>
             </TableCell>
-            <TableCell align="center">
+            <TableCell align="left" className={classes.cell}>
               <b>所需</b>
             </TableCell>
           </TableRow>
@@ -59,8 +64,8 @@ export default function MaterialEstimation({
           {Object.entries(materials).map(([name, material]) => {
             return (
               <TableRow key={name}>
-                <TableCell className={classes.cell}>
-                  <div className={classes.iconCell}>
+                <TableCell className={classes.iconCell}>
+                  <div className={classes.iconDiv}>
                     <img
                       alt={name + " icon"}
                       className={classes.image}
@@ -71,7 +76,7 @@ export default function MaterialEstimation({
                     </Typography>
                   </div>
                 </TableCell>
-                <TableCell align="center" className={classes.cell}>
+                <TableCell align="left" className={classes.cell}>
                   <TextField
                     name={name}
                     className={classes.input}
@@ -82,7 +87,7 @@ export default function MaterialEstimation({
                     value={inventory[name] || ""}
                   />
                 </TableCell>
-                <TableCell align="center" className={classes.cell}>
+                <TableCell align="left" className={classes.cell}>
                   {inventory[material.name]
                     ? material.quantity - inventory[material.name] >= 0
                       ? material.quantity - inventory[material.name]
