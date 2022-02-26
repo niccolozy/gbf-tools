e14690ee9caf7b600e53df879b700c34f324ac7e
niccolozy <zy_graysky@hotmail.com>
2022-02-26 17:50:01 +0000

    Deploying to gh-pages from @ niccolozy/gbf-tools@3b3b5de30affb5a1d064080e2801a64f7b6c7c90 🚀

--- README.md
+++
@@ -1,5 +0,0 @@
-This project was bootstrapped with [Create React App](https://github.com/facebook/create-react-app).
-
-## Readme
-
-a set of useful tools (planned) for gbf hosted at https://niccolozy.github.io/gbf-tools/
--- src/assets/materials/coop/index.js
+++
@@ -1,6 +0,0 @@
-export const Evil_Judge_Crystal = require("./Evil_Judge_Crystal.jpg");
-export const Frozen_Hell_Prism = require("./Frozen_Hell_Prism.jpg");
-export const Halo_Light_Quartz = require("./Halo_Light_Quartz.jpg");
-export const Horseman_Plate = require("./Horseman's_Plate.jpg");
-export const Infernal_Garnet = require("./Infernal_Garnet.jpg");
-export const Phantom_Demon_Jewel = require("./Phantom_Demon_Jewel.jpg");
